878d874803aea029f795b0b11ab6ae5a7a19ecb6
faraqhan <f.faraqhan@gmail.com>
2018-09-26 01:52:12 +0300

    add tests for validate_user_name method

--- src/users.py
+++ src/users.py
@@ -4,7 +4,10 @@ import re
 def validate_password(password):
     if not password:
         return "Please fill missing password"
-    if re.match(re.compile(r'([A-Z][a-z][0-9][!@#$%&*]$)'), password):
+    if re.match(re.compile(r'([A-Z][a-z][0-9][!@#$%&*])'), password):
         return password
     return 'Password format is wrong'
 
+
+def validate_user_name(user_name):
+    pass
